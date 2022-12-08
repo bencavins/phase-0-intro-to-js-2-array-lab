9606cce865c2229f5fb1c95c0f0689b75be1dce5
Ben Cavins <ben.cavins@flatironschool.com>
2022-12-08 11:34:01 -0500

    commit tree 49cefad72089115038094178578ffa2b6be1b23e

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
+
